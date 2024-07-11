a092af1df9c344e4ef5f2b71d4fd94f9f3a6d47c
ChoSeyoung <a01075511157@gmail.com>
2024-07-11 23:32:30 +0900

    init

---
+++ src/main/java/my/trader/coin/CoinApplication.java
@@ -0,0 +1,13 @@
+package my.trader.coin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/trader/coin/CoinApplicationTests.java
@@ -0,0 +1,13 @@
+package my.trader.coin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
