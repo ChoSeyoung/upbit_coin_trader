7cff412048bf58cc311f831f019b5fc079a2ff4a
ChoSeyoung <a01075511157@gmail.com>
2024-10-19 21:00:47 +0900

    add file for koyeb deploy

--- src/main/java/my/trader/coin/dto/exchange/OpenOrderRequestDto.java
+++ src/main/java/my/trader/coin/dto/exchange/OpenOrderRequestDto.java
@@ -13,9 +13,19 @@ import java.util.List;
 @AllArgsConstructor
 public class OpenOrderRequestDto {
   private String market;
+
+  @Builder.Default
   private String state = "wait";
+
+  @Builder.Default
   private List<String> states = List.of("wait");
+
+  @Builder.Default
   private Integer page = 1;
+
+  @Builder.Default
   private Integer limit = 100;
+
+  @Builder.Default
   private String orderBy = "desc";
 }
